1124ed81777d77e771f349eced406ee19e07f778
nagaraj <raj.nagaraj1990@gmail.com>
2025-02-17 18:13:36 -0500

    fix: update image

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t nagaraj23/local-llm-api:latest . | tee deploy.txt
-
-docker push nagaraj23/local-llm-api:latest | tee install_output.txt
+docker tag nagaraj23/local-llm-api:latest ghcr.io/nagaraj-real/local-llm-api:latest
+docker push ghcr.io/nagaraj-real/local-llm-api:latest | tee install_output.tx
